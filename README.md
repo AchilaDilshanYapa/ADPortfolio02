f61c709f2e8213d633927e537aed6d29ee807345
AchilaDilshanYapa <achiladilshan224455@gmail.com>
2025-02-10 01:43:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+# AchilaDilshanYapaPortfolioReact
+React 
+=======
+### Description
+
+AD King
+>>>>>>> 7c1c9c3 (Initial commit)
---
+++ src/content_option.js
@@ -0,0 +1,151 @@
+const logotext = "AD King";
+const meta = {
+    title: "Achila Yapa",
+    description: "I’m Achila Dilshan Yapa,Undergraduate Software Engineering student.",
+};
+
+
+const introdata = {
+    title: "I’m Achila Dilshan Yapa",
+    animated: {
+        first: "I'm an Undergraduate Software Engineering Student.",
+        second: "I love coding",
+        third: "I code cool websites",
+    },
+    description: "Crafting intelligent solutions with code & creativity. 🚀 Dedicated to building innovative and efficient software as a passionate Software Engineer!",
+    your_img_url: require("./assets/me1.jpg"),
+};
+
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I’m Achila Dilshan Yapa, a passionate Software Engineer with a deep love for AI, web development, and creative technologies. 🚀 I specialize in building intelligent solutions using React, Next.js, and AI-powered applications. Beyond coding, I explore design, animation, and networking, always pushing boundaries to create something extraordinary!",
+};
+const worktimeline = [{
+        jobtitle: "Desining Portfolio",
+        where: "Achila Dilshan",
+        date: "2025",
+    },
+    
+];
+
+const skills = [
+    {
+        name: "HTML, CSS, Tailwind CSS ",
+        value: 90,
+    },
+    {
+        name: "React.js & Next.js",
+        value: 60,
+    },
+    {
+        name: "JavaScript & TypeScript",
+        value: 75,
+    },
+    {
+        name: "AI & Automation ",
+        value: 40,
+    },
+    {
+        name: "java ",
+        value: 55,
+    },
+    {
+        name: "UI/UX Design (Figma, Adobe XD)",
+        value: 70,
+    },
+    {
+        name: "Video Editing (Premiere Pro, After Effects)",
+        value: 65,
+    },
+    {
+        name: "3D Design (Blender, Unreal Engine))",
+        value: 45,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design🎨",
+        description: "Design is more than just looks—it’s about creating seamless user experiences. I specialize in designing clean, modern interfaces using Figma and Adobe XD. From wireframing to full-fledged prototypes, I ensure your digital products are both functional and visually stunning.",
+    },
+    {
+        title: " Web Development💻",
+        description: "A well-designed website is crucial for businesses, brands, and personal portfolios. I build responsive, high-performance websites using React, Next.js, and other modern technologies. My focus is on delivering fast, SEO-optimized, and visually appealing designs that enhance user experience.",
+    },
+    {
+        title: "Video Editing & Motion Graphics🎬",
+        description: "A compelling video can tell your story in ways that words can’t. I offer professional video editing, color grading, and motion graphics using Adobe Premiere Pro and After Effects. Whether it’s a YouTube intro, a promotional video, or cinematic editing, I bring creativity to every project.",
+    },
+    {
+        title: " 3D Design & Animation🖥️",
+        description: "I work with Blender and Unreal Engine to create 3D models, animations, and VFX for various projects. Whether you need a product showcase, architectural visualization, or an animated logo, I can bring your ideas to life in a visually stunning way.",
+    },
+    {
+        title: "AI & Automation🤖",
+        description: "AI is transforming the digital world, and I help businesses integrate AI-driven solutions. Whether it’s AI-powered video generation, automation scripts, or machine learning applications, I create intelligent solutions that enhance efficiency and productivity.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "achiladilshanyapa@gmail.com",
+    YOUR_FONE: "+9474 00 99 338",
+    description: "Excited to connect and create! 🚀 Whether it's a new project, collaboration, or just a tech chat—I'm all ears. Let's make something incredible together!",
+    // Add AD King email
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_fckyxb6",
+    YOUR_TEMPLATE_ID: "template_2f2xonc",
+    YOUR_USER_ID: "Xfg4IkCXk1YgXWX2L",
+};
+
+const socialprofils = {
+    github: "http://github.com/AchilaDilshanYapa",
+    facebook: " http://www.facebook.com/achila.yapa?mibextid=ZbWKwL",
+    linkedin: "http://www.linkedin.com/in/achilayapa",
+    twitter: "http://x.com/AchilaYapa?s=09",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
